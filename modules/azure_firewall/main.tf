4ff7797adae42e2a8998d51d39aa23140db8e5cc
Ben Roberts <mallonox@hotmail.com>
2022-07-25 04:31:19 +0000

    fixed firewall subnet name

--- modules/azure_firewall/main.tf
+++ modules/azure_firewall/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "rg2" {
 
 # Create a firewall subnet
 resource "azurerm_subnet" "subnet2" {
-  name                 = "firewall_subnet"
+  name                 = "AzureFirewallSubnet"
   resource_group_name  = azurerm_resource_group.rg2.name
   virtual_network_name = azurerm_virtual_network.vnet2.name
   address_prefixes     = ["10.1.1.0/24"]
